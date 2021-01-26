031237b2879622463d05c26542c6ecc0dc879b48
Mark Clement <clement@cs.byu.edu>
2021-01-26 14:35:53 -0700

    Update vector-access-middle.cpp

--- vector-access-middle.cpp
+++ vector-access-middle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
     vector<string> mystrings;
-    int n = 40000; // The number of inserts
+    int n = 40000000; // The number of inserts
     for(int i = 0; i < n; i++) {
         mystrings.insert(mystrings.end(),"Hello");
     }
