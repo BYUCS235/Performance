a3f1af02b683ca4cc88d9cce05a3cfb739ef1a99
Mark Clement <clement@cs.byu.edu>
2021-01-26 14:32:42 -0700

    Create vector-access-middle.cpp

---
+++ vector-access-middle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<string> mystrings;
+    int n = 40000; // The number of inserts
+    for(int i = 0; i < n; i++) {
+        mystrings.insert(mystrings.end(),"Hello");
+    }
+    string value;
+    for(int i = 0; i < n; i++) {
+        vector<string>::iterator middle = mystrings.begin();
+        advance(middle,n/2);
+        value = *middle;
+    } 
+}
