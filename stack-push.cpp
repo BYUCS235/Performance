aff0708a20814bdfb6f83d2d349d1c0caa1ee248
Mark Clement <clement@cs.byu.edu>
2021-01-26 14:44:43 -0700

    Create stack-push.cpp

---
+++ stack-push.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    int n = 4000000;
+		stack<string> mystrings;
+    for(int i = 0; i < n; i++) {
+        mystrings.push("Hello");
+    }
+}
